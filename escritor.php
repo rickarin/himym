1c5b68a7a84deb421f0cedca6d28a85f39de4e1f
Barbara Chagas <60980705+barbarachagas@users.noreply.github.com>
2022-02-28 11:09:33 -0300

    Initial commit

---
+++ escritor.php
@@ -0,0 +1,43 @@
+<?php include ('header.php');?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <img src="img/Rectangle 2.png" alt="" class="temporada__img">
+        </div>
+        <div class="col-md-4 titulo">
+            Escritor
+        </div>
+    </div>
+<div class="accordion" id="">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Nome do escritor
+    </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        <table>
+            <thead>
+                <tr>
+                    <th>temporada</th>
+                    <th>número</th>
+                    <th>episódio</th>
+                    <th>data</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>01</td>
+                    <td>01</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat labore nisi deserunt totam quas, id eius inventore dignissimos incidunt accusantium autem porro, nihil, debitis esse quam voluptatibus. Iusto, nulla quidem!</td>
+                    <td>21/02/2022</td>
+                </tr>
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include ('footer.php');?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include ('header.php');?>
+
+    <div class="dados">
+        <section class="temporada">
+            <a href="temporadas.php">
+                <img src="img/Rectangle 1.png" alt="" class="temporada__img">
+                <p class="temporada__txt">temporadas</p>
+            </a>
+        </section>
+        <section class="diretor">
+            <img src="img/Rectangle 2.png" alt="" class="diretor__img">
+            <p class="diretor__txt">diretor</p>
+        </section>
+        <section class="escritor">
+            <img src="img/Rectangle 3.png" alt="" class="escritor__img">
+            <p class="escritor__txt">escritor</p>
+        </section>
+        <section class="nota">
+            <img src="img/Rectangle 4.png" alt="" class="nota__img">
+            <p class="nota__txt">nota IMDB</p>
+        </section>
+        <section class="data">
+            <img src="img/Rectangle 5.png" alt="" class="data__img">
+            <p class="data__txt">data em que foi ao ar</p>
+        </section>
+        <section class="visualizacoes">
+            <img src="img/Rectangle 6.png" alt="" class="visualizacoes__img">
+            <p class="visualizacoes__txt">quantidade de visualizações</p>
+        </section>
+    </div>
+</body>
+</html>
